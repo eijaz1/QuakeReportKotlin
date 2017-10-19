21af6dc3c017a38a9fd7e72c3283fc42a13b66a6
Eijaz Allibhai <eijazallibhai@gmail.com>
2017-10-19 18:00:32 +0100

    AsyncTaskLoader added

--- QuakeReport/app/src/main/java/com/example/android/quakereport/EarthquakeActivity.kt
+++ QuakeReport/app/src/main/java/com/example/android/quakereport/EarthquakeActivity.kt
@@ -15,63 +15,63 @@
  */
 package com.example.android.quakereport
 
-import android.os.AsyncTask
+import android.content.Loader
+import android.app.LoaderManager.LoaderCallbacks
 import android.os.Bundle
 import android.support.v7.app.AppCompatActivity
 import android.support.v7.widget.LinearLayoutManager
 import android.support.v7.widget.RecyclerView
 import java.util.ArrayList
 
-class EarthquakeActivity : AppCompatActivity() {
+class EarthquakeActivity : AppCompatActivity(), LoaderCallbacks<ArrayList<Earthquake>> {
 
-    private val USGS_REQUEST_URL: String = "https://earthquake.usgs.gov/fdsnws/event/1/query?format=geojson&orderby=time&minmag=3&limit=20"
-
-    /** Adapter for the list of earthquakes  */
-    private val mAdapter: EarthquakeAdapter? = null
+    /**
+     * Constant value for the earthquake loader ID. We can choose any integer.
+     * This really only comes into play if you're using multiple loaders.
+     */
+    val EARTHQUAKE_LOADER_ID: Int = 1
+    val USGS_REQUEST_URL: String = "https://earthquake.usgs.gov/fdsnws/event/1/query?format=geojson&orderby=time&minmag=3&limit=20"
 
     //initialize @earthquake_list with type recyclerView
     //iniialize @earthquakes with type ArrayList of Earthquake objects
-    lateinit var earthquake_list : RecyclerView
+    lateinit var earthquake_list: RecyclerView
     var earthquakes:ArrayList<Earthquake> = ArrayList()
+
+    override fun onCreateLoader(p0: Int, p1: Bundle?): Loader<ArrayList<Earthquake>> {
+
+        val result = EarthquakeLoader(this, USGS_REQUEST_URL)
+        return result
+    }
+
+    override fun onLoadFinished(p0: Loader<ArrayList<Earthquake>>?, earthquakes: ArrayList<Earthquake>?) {
+
+        if (earthquakes !=null && !earthquakes.isEmpty()) {
+
+            //setting up RecyclerView to recycle earthquake_list_item in
+            // rv_earthquake_list for each earthquake
+            earthquake_list = findViewById(R.id.rv_earthquake_list) as RecyclerView
+            earthquake_list.layoutManager = LinearLayoutManager(this@EarthquakeActivity)
+            earthquake_list.adapter = EarthquakeAdapter(earthquakes, this@EarthquakeActivity)
+        }
+    }
+
+    override fun onLoaderReset(p0: Loader<ArrayList<Earthquake>>?) {
+        earthquakes.clear()
+    }
+
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.earthquake_activity)
+
+        // Interact with loader
+        getLoaderManager().initLoader(EARTHQUAKE_LOADER_ID, null, this)
 
         //setting up RecyclerView to recycle earthquake_list_item in
         // rv_earthquake_list for each earthquake
         earthquake_list = findViewById(R.id.rv_earthquake_list) as RecyclerView
         earthquake_list.layoutManager = LinearLayoutManager(this)
         earthquake_list.adapter = EarthquakeAdapter(earthquakes, this)
-
-        // Start the AsyncTask to fetch the earthquake data
-        val task: EarthquakeAsyncTask = EarthquakeAsyncTask()
-        task.execute(USGS_REQUEST_URL)
-
-    }
-
-    inner class EarthquakeAsyncTask : AsyncTask<String, Void, ArrayList<Earthquake>>() {
-
-        override fun doInBackground(vararg urls: String?): ArrayList<Earthquake>? {
-
-            // Don't perform the request if there are no URLs, or the first URL is null.
-            if (urls.size < 1 || urls[0] == null) {
-                return null
-            }
-
-            //list of earthquakes pulled from JSON response
-            earthquakes = QueryUtils.fetchEarthquakeData(USGS_REQUEST_URL) as ArrayList<Earthquake>
-            return earthquakes
-        }
-
-        override fun onPostExecute(earthquakes: ArrayList<Earthquake>?) {
-            //setting up RecyclerView to recycle earthquake_list_item in
-            // rv_earthquake_list for each earthquake
-            earthquake_list = findViewById(R.id.rv_earthquake_list) as RecyclerView
-            earthquake_list.layoutManager = LinearLayoutManager(this@EarthquakeActivity)
-            earthquake_list.adapter = EarthquakeAdapter(earthquakes!!, this@EarthquakeActivity)
-
-        }
 
     }
 
---
+++ QuakeReport/app/src/main/java/com/example/android/quakereport/EarthquakeLoader.kt
@@ -0,0 +1,44 @@
+package com.example.android.quakereport
+
+import android.content.AsyncTaskLoader
+import android.content.Context
+import com.example.android.quakereport.QueryUtils.fetchEarthquakeData
+
+/**
+ * Loads a list of earthquakes by using an AsyncTask to perform the
+ * network request to the given URL.
+ */
+
+/**
+ * Constructs a new [EarthquakeLoader].
+
+ * @param context of the activity
+ * *
+ * @param url to load data from
+ */
+
+class EarthquakeLoader(context: Context, val url: String?) : AsyncTaskLoader<ArrayList<Earthquake>>(context) {
+
+    override fun onStartLoading() {
+        forceLoad()
+    }
+
+    /**
+     * This is on a background thread.
+     */
+    override fun loadInBackground(): ArrayList<Earthquake>? {
+        if (url == null) {
+            return null
+        }
+
+        // Perform the network request, parse the response, and extract a list of earthquakes.
+        var earthquakes: ArrayList<Earthquake> = fetchEarthquakeData(url)
+        return earthquakes
+    }
+
+    companion object {
+
+        /** Tag for log messages  */
+        private val LOG_TAG = EarthquakeLoader::class.java.name
+    }
+}
